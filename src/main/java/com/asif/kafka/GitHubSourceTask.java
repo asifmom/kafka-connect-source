97b9f5947c8b021172cab7a0f555781bbd8c9099
Asif Momin <asifdmomin@gmail.com>
2020-09-30 18:29:37 +0530

    added github source task

--- src/main/java/com/asif/kafka/GitHubSourceTask.java
+++ src/main/java/com/asif/kafka/GitHubSourceTask.java
@@ -3,45 +3,138 @@ package com.asif.kafka;
 import com.asif.kafka.model.Issue;
 import com.asif.kafka.model.PullRequest;
 import com.asif.kafka.model.User;
+import com.asif.kafka.utils.DateUtils;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
-import com.github.jcustenborder.kafka.connect.utils.VersionUtil;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
+import java.time.Instant;
+import java.util.*;
 import static com.asif.kafka.GitHubSchemas.*;
 
+
 public class GitHubSourceTask extends SourceTask {
-  /*
-    Your connector should never use System.out for logging. All of your classes should use slf4j
-    for logging
- */
-  static final Logger log = LoggerFactory.getLogger(GitHubSourceTask.class);
+  private static final Logger log = LoggerFactory.getLogger(GitHubSourceTask.class);
+  public GitHubSourceConnectorConfig config;
+
+  protected Instant nextQuerySince;
+  protected Integer lastIssueNumber;
+  protected Integer nextPageToVisit = 1;
+  protected Instant lastUpdatedAt;
+
+  GitHubAPIHttpClient gitHubHttpAPIClient;
 
   @Override
   public String version() {
-    return VersionUtil.version(this.getClass());
+    return VersionUtil.getVersion();
   }
 
   @Override
   public void start(Map<String, String> map) {
-    //TODO: Do things here that are required to start your task. This could be open a connection to a database, etc.
+    //Do things here that are required to start your task. This could be open a connection to a database, etc.
+    config = new GitHubSourceConnectorConfig(map);
+    initializeLastVariables();
+    gitHubHttpAPIClient = new GitHubAPIHttpClient(config);
   }
+
+  private void initializeLastVariables(){
+    Map<String, Object> lastSourceOffset = null;
+    lastSourceOffset = context.offsetStorageReader().offset(sourcePartition());
+    if( lastSourceOffset == null){
+      // we haven't fetched anything yet, so we initialize to 7 days ago
+      nextQuerySince = config.getSince();
+      lastIssueNumber = -1;
+    } else {
+      Object updatedAt = lastSourceOffset.get(UPDATED_AT_FIELD);
+      Object issueNumber = lastSourceOffset.get(NUMBER_FIELD);
+      Object nextPage = lastSourceOffset.get(NEXT_PAGE_FIELD);
+      if(updatedAt != null && (updatedAt instanceof String)){
+        nextQuerySince = Instant.parse((String) updatedAt);
+      }
+      if(issueNumber != null && (issueNumber instanceof String)){
+        lastIssueNumber = Integer.valueOf((String) issueNumber);
+      }
+      if (nextPage != null && (nextPage instanceof String)){
+        nextPageToVisit = Integer.valueOf((String) nextPage);
+      }
+    }
+  }
+
+
 
   @Override
   public List<SourceRecord> poll() throws InterruptedException {
-    //TODO: Create SourceRecord objects that will be sent the kafka cluster.
-    throw new UnsupportedOperationException("This has not been implemented.");
+    gitHubHttpAPIClient.sleepIfNeed();
+
+    // fetch data
+    final ArrayList<SourceRecord> records = new ArrayList<>();
+    JSONArray issues = gitHubHttpAPIClient.getNextIssues(nextPageToVisit, nextQuerySince);
+    // we'll count how many results we get with i
+    int i = 0;
+    for (Object obj : issues) {
+      Issue issue = Issue.fromJson((JSONObject) obj);
+      SourceRecord sourceRecord = generateSourceRecord(issue);
+      records.add(sourceRecord);
+      i += 1;
+      lastUpdatedAt = issue.getUpdatedAt();
+    }
+    if (i > 0) log.info(String.format("Fetched %s record(s)", i));
+    if (i == 100){
+      // we have reached a full batch, we need to get the next one
+      nextPageToVisit += 1;
+    }
+    else {
+      nextQuerySince = lastUpdatedAt.plusSeconds(1);
+      nextPageToVisit = 1;
+      gitHubHttpAPIClient.sleep();
+    }
+    return records;
+  }
+
+  private SourceRecord generateSourceRecord(Issue issue) {
+    return new SourceRecord(
+            sourcePartition(),
+            sourceOffset(issue.getUpdatedAt()),
+            config.getTopic(),
+            null, // partition will be inferred by the framework
+            KEY_SCHEMA,
+            buildRecordKey(issue),
+            VALUE_SCHEMA,
+            buildRecordValue(issue),
+            issue.getUpdatedAt().toEpochMilli());
   }
 
   @Override
   public void stop() {
-    //TODO: Do whatever is required to stop your task.
+    // Do whatever is required to stop your task.
+  }
+
+  private Map<String, String> sourcePartition() {
+    Map<String, String> map = new HashMap<>();
+    map.put(OWNER_FIELD, config.getOwnerConfig());
+    map.put(REPOSITORY_FIELD, config.getRepoConfig());
+    return map;
+  }
+
+  private Map<String, String> sourceOffset(Instant updatedAt) {
+    Map<String, String> map = new HashMap<>();
+    map.put(UPDATED_AT_FIELD, DateUtils.MaxInstant(updatedAt, nextQuerySince).toString());
+    map.put(NEXT_PAGE_FIELD, nextPageToVisit.toString());
+    return map;
+  }
+
+  private Struct buildRecordKey(Issue issue){
+    // Key Schema
+    Struct key = new Struct(KEY_SCHEMA)
+            .put(OWNER_FIELD, config.getOwnerConfig())
+            .put(REPOSITORY_FIELD, config.getRepoConfig())
+            .put(NUMBER_FIELD, issue.getNumber());
+
+    return key;
   }
 
   public Struct buildRecordValue(Issue issue){
@@ -73,4 +166,6 @@ public class GitHubSourceTask extends SourceTask {
     }
 
     return valueStruct;
-  }}
+  }
+
+}
